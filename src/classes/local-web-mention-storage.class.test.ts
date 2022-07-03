54f014c6e98a1f37c5a4758c0b8e4c1bfc32d099
vandie <mikevdv@vandie.co.uk>
2022-07-03 23:00:38 +0100

    feat: add tests for completed parts of the storage class

---
+++ src/classes/local-web-mention-storage.class.test.ts
@@ -0,0 +1,42 @@
+import { LocalWebMentionStorage } from './local-web-mention-storage.class';
+let localWebMentionStorage: LocalWebMentionStorage;
+describe('LocalWebMentionStorage', () => {
+  beforeEach(() => {
+    localWebMentionStorage = new LocalWebMentionStorage();
+  });
+
+  describe('addPendingMention', () => {
+    it('correctly adds a pending mention', async () => {
+      expect((localWebMentionStorage as any).queue).toEqual([]);
+      await localWebMentionStorage.addPendingMention({
+        source: 'http://example.com/example',
+        target: 'http://example.org/example'
+      });
+      expect((localWebMentionStorage as any).queue).toEqual([{
+        source: 'http://example.com/example',
+        target: 'http://example.org/example'
+      }]);
+    });
+  });
+
+  describe('getNextPendingMentions', () => {
+    it('should return all pending mentions when called', async () => {
+      for(let i = 0; i < 10; i++) {
+        localWebMentionStorage.addPendingMention({
+          source: `http://example.com/example/${i}`,
+          target: 'http://example.org/example'
+        });
+      }
+      expect((localWebMentionStorage as any).queue.length).toEqual(10);
+      const mentions = await localWebMentionStorage.getNextPendingMentions();
+      expect((localWebMentionStorage as any).queue.length).toEqual(0);
+      expect(mentions.length).toEqual(10);
+      for(let i = 0; i < 10; i++) {
+        expect(mentions[i]).toEqual({
+          source: `http://example.com/example/${i}`,
+          target: 'http://example.org/example'
+        });
+      }
+    });
+  });
+});
