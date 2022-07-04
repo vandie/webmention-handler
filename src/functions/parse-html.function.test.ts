44d34105f7b20c6b85735c9e6703e143aa3f0adc
vandie <mikevdv@vandie.co.uk>
2022-07-04 12:08:51 +0100

    feat: add missing test

--- src/functions/parse-html.function.test.ts
+++ src/functions/parse-html.function.test.ts
@@ -11,7 +11,7 @@ describe('parseMicroformats', () => {
 
   it('should parse replies with an author', () => {
     const testHtml = fs.readFileSync(path.join(__dirname, '../test-data/html-reply-with-author.html'), 'utf8');
-    const comment = [{"properties": {"author": [{"properties": {"name": [""], "photo": ["http://mysite.example.org/icon.jpg"], "url": ["http://mysite.example.org"]}, "type": ["h-card"], "value": ""}], "content": ["Good point! Now what is the next thing we should do?"], "in-reply-to": ["http://example.com/note123"], "name": ["Good point! Now what is the next thing we should do?"]}, "type": ["h-entry"]}, {"properties": {"mention-of": ["http://mysite.example.org", "http://example.com/note123"]}, "type": ["h-mention-of"]}];
+    const comment = [{"properties": {"author": [{"properties": {"name": [""], "photo": ["http://mysite.example.org/icon.jpg"], "url": ["http://mysite.example.org"]}, "type": ["h-card"], "value": ""}], "content": ["Good point! Now what is the next thing we should do?"], "in-reply-to": ["http://example.com/note123"], "name": ["Good point! Now what is the next thing we should do?"]}, "type": ["h-entry"]}, {"properties": {"mention-of": ["http://mysite.example.org", "http://example.com/note123", "http://mysite.example.org/icon.jpg",]}, "type": ["h-mention-of"]}];
     expect(parseHtml(testHtml, 'http://example.com')).toEqual(comment);
   });
 })
