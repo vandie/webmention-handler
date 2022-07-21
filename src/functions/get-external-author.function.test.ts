957bf3bb7a13f9ed8407e0b132adc2d4cf1671a3
vandie <mikevdv@vandie.co.uk>
2022-07-21 15:37:47 +0100

    fix: neaten tests

--- src/functions/get-external-author.function.test.ts
+++ src/functions/get-external-author.function.test.ts
@@ -1,15 +1,30 @@
+import path from "path";
+import fs from "fs";
 import { getExternalAuthor } from "./get-external-author.function";
+
 let fetchMock:jest.SpyInstance;
 describe('getExternalAuthor', () => {
   beforeEach(() => {
+    jest.clearAllMocks();
     fetchMock = jest.spyOn(require('./fetch-html.function'), 'fetchHtml').mockImplementation((url) => {
-      if(url === 'https://example.org') return {status: 200, html: `<html><body><div class="h-card"><img class="u-photo" src="https://example.org/images/mike.png" alt="Michael Walter Van Der Velden"><h1><a href="/" class="u-url p-name">Michael Walter Van Der Velden</a></h1></div></body></html>`};
+      if(url === 'https://example.org') return {
+        status: 200,
+        html: fs.readFileSync(path.join(__dirname, '../test-data/html-author.html'), 'utf8')
+    };
       return {error: '404'};
     });
-  })
+  });
   it('fetches an author correctly', async () => {
     const author = await getExternalAuthor('https://example.org');
-    const expected = {"name": "Michael Walter Van Der Velden", "photo": {"alt": "Michael Walter Van Der Velden", "value": "https://example.org/images/mike.png"}, "url": "https://example.org/", "type": "h-card"};
+    const expected = {
+      "name": "Michael Walter Van Der Velden",
+      "photo": {
+        "alt": "Michael Walter Van Der Velden",
+        "value": "https://example.org/images/mike.png"
+      },
+      "url": "https://example.org/",
+      "type": "h-card"
+    };
     expect(author).toEqual(expected);
     expect(fetchMock).toHaveBeenCalledWith('https://example.org');
   });
--- src/functions/normalize-authors.function.test.ts
+++ src/functions/normalize-authors.function.test.ts
@@ -1,44 +1,55 @@
+import path from "path";
+import fs from "fs";
 import { normalizeAuthors } from "./normalize-authors.function";
 
 let fetchMock:jest.SpyInstance;
+const exampleJSON = {
+  "name": "Michael Walter Van Der Velden",
+  "photo": {
+    "alt": "Michael Walter Van Der Velden",
+    "value": "https://example.org/images/mike.png"
+  },
+  "url": "https://example.org/",
+  "type": "h-card"
+};
+
 describe('normalizeAuthors', () => {
   beforeEach(() => {
     jest.clearAllMocks();
     fetchMock = jest.spyOn(require('./fetch-html.function'), 'fetchHtml').mockImplementation((url) => {
-      if(url === 'https://example.org') return {status: 200, html: `<html><body><div class="h-card"><img class="u-photo" src="https://example.org/images/mike.png" alt="Michael Walter Van Der Velden"><h1><a href="/" class="u-url p-name">Michael Walter Van Der Velden</a></h1></div></body></html>`};
+      if(url === 'https://example.org') return {
+        status: 200,
+        html: fs.readFileSync(path.join(__dirname, '../test-data/html-author.html'), 'utf8')
+    };
       return {error: '404'};
     });
   });
 
   it('correctly handles a single author', async () => {
     const authors = await normalizeAuthors('https://example.org');
-    const expected = {"name": "Michael Walter Van Der Velden", "photo": {"alt": "Michael Walter Van Der Velden", "value": "https://example.org/images/mike.png"}, "url": "https://example.org/", "type": "h-card"};
-    expect(authors).toEqual(expected);
+    expect(authors).toEqual(exampleJSON);
     expect(fetchMock).toHaveBeenCalledTimes(1);
     expect(fetchMock).toHaveBeenCalledWith('https://example.org');
   });
 
   it('correctly handles multiple authors', async () => {
     const authors = await normalizeAuthors(['https://example.org', 'https://example.org']);
-    const expected = {"name": "Michael Walter Van Der Velden", "photo": {"alt": "Michael Walter Van Der Velden", "value": "https://example.org/images/mike.png"}, "url": "https://example.org/", "type": "h-card"};
-    expect(authors).toEqual([expected, expected]);
+    expect(authors).toEqual([exampleJSON, exampleJSON]);
     expect(fetchMock).toHaveBeenCalledTimes(2);
     expect(fetchMock).toHaveBeenCalledWith('https://example.org');
   });
 
   it('correctly handles multiple authors where one is invalid', async () => {
     const authors = await normalizeAuthors(['https://example.org', 'https://example.com']);
-    const expected = {"name": "Michael Walter Van Der Velden", "photo": {"alt": "Michael Walter Van Der Velden", "value": "https://example.org/images/mike.png"}, "url": "https://example.org/", "type": "h-card"};
-    expect(authors).toEqual([expected, 'https://example.com']);
+    expect(authors).toEqual([exampleJSON, 'https://example.com']);
     expect(fetchMock).toHaveBeenCalledTimes(2);
     expect(fetchMock).toHaveBeenCalledWith('https://example.org');
     expect(fetchMock).toHaveBeenCalledWith('https://example.com');
   });
 
   it('correctly handles an already normalized author', async () => {
-    const expected = {"name": "Michael Walter Van Der Velden", "photo": {"alt": "Michael Walter Van Der Velden", "value": "https://example.org/images/mike.png"}, "url": "https://example.org/", "type": "h-card"};
-    const authors = await normalizeAuthors(expected);
-    expect(authors).toEqual(expected);
+    const authors = await normalizeAuthors(exampleJSON);
+    expect(authors).toEqual(exampleJSON);
     expect(fetchMock).not.toHaveBeenCalled();
   });
 });
